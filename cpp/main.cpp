43147c49acae7c92eaed4ea6af2f5e91c60c4824
binhnguyen288-source <nguyenthebinh288@gmail.com>
2022-08-13 18:48:25 +0700

    Refactor projection code

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -41,37 +41,81 @@ inline std::pair<float, float> mapsToCube(float x, float y, float z) {
 }
 
 
+template<typename T>
+struct Vec3 {
+    T x, y, z;
+};
+
+struct Mat3 {
+    float m[9];
+
+    Mat3 operator*(Mat3 const& other) const {
+        Mat3 res;
+
+        res.m[0] = m[0] * other.m[0] + m[1] * other.m[3] + m[2] * other.m[6];
+        res.m[1] = m[0] * other.m[1] + m[1] * other.m[4] + m[2] * other.m[7];
+        res.m[2] = m[0] * other.m[2] + m[1] * other.m[5] + m[2] * other.m[8];
+
+        res.m[3] = m[3] * other.m[0] + m[4] * other.m[3] + m[5] * other.m[6];
+        res.m[4] = m[3] * other.m[1] + m[4] * other.m[4] + m[5] * other.m[7];
+        res.m[5] = m[3] * other.m[2] + m[4] * other.m[5] + m[5] * other.m[8];
+
+        res.m[6] = m[6] * other.m[0] + m[7] * other.m[3] + m[8] * other.m[6];
+        res.m[7] = m[6] * other.m[1] + m[7] * other.m[4] + m[8] * other.m[7];
+        res.m[8] = m[6] * other.m[2] + m[7] * other.m[5] + m[8] * other.m[8];
+
+        return res;
+    }
+
+    template<typename T>
+    Vec3<float> operator*(Vec3<T> const& v) const {
+        return {
+            m[0] * v.x + m[1] * v.y + m[2] * v.z,
+            m[3] * v.x + m[4] * v.y + m[5] * v.z,
+            m[6] * v.x + m[7] * v.y + m[8] * v.z,
+        };
+    }
+};
+
+
+
 extern "C" void viewerQuery(
     std::uint8_t* dst, 
     int dstWidth, int dstHeight, float theta0, float phi0, float hfov
 ) {
 
-    float const aspectRatio = (float)dstHeight / dstWidth;
     float const f           = std::tan(hfov / 2);
-    float const vfov        = 2 * std::atan(aspectRatio * f);
-    float const incY        = 2 * f / dstWidth;
-    const int nCubeSide = currentCubemap.cols;
+    const int nCubeSide     = currentCubemap.cols;
 
     using std::cos;
     using std::sin;
 
-    const float Rot[9] = {
-        cos(phi0) * cos(theta0), -sin(theta0), sin(phi0) * cos(theta0),
-        cos(phi0) * sin(theta0), cos(theta0), sin(phi0) * sin(theta0),
+
+    const Mat3 RotZ{
+        cos(theta0), -sin(theta0), 0,
+        sin(theta0), cos(theta0) , 0,
+        0          , 0           , 1
+    };
+
+    const Mat3 RotY{
+        cos(phi0) , 0, sin(phi0),
+        0         , 1, 0,
         -sin(phi0), 0, cos(phi0)
     };
 
+    const Mat3 projection{
+        2 * f / dstWidth, 0               , -f * dstHeight / dstWidth,
+        0               , 2 * f / dstWidth, -f,
+        0               , 0               , 1
+    };
+
+    const Mat3 transform = RotZ * RotY * projection;
+
 
     for (int i = 0; i < dstHeight; ++i) {
-        const float XonPlane = aspectRatio * f * (2.0f * i / dstHeight - 1.0f);
-        float YonPlane = -f; // initial Yonplane
-        constexpr float ZonPlane = 1;
-
-              float Rotx = XonPlane * Rot[0] + YonPlane * Rot[1] + ZonPlane * Rot[2];
-              float Roty = XonPlane * Rot[3] + YonPlane * Rot[4] + ZonPlane * Rot[5];
-        const float Rotz = XonPlane * Rot[6] + YonPlane * Rot[7] + ZonPlane * Rot[8];
-
         for (int j = 0; j < dstWidth; ++j) {
+
+            auto const [Rotx, Roty, Rotz] = transform * Vec3<int>{i, j, 1};
 
 
             auto [srci, srcj] = mapsToCube(Rotx, Roty, Rotz);
@@ -84,9 +128,6 @@ extern "C" void viewerQuery(
 
             std::copy_n(srcPixelRGB, 3, dstPixelRGBA);
             dstPixelRGBA[3] = 255;
-            
-            Rotx += incY * Rot[1];
-            Roty += incY * Rot[4];
 
         }
     }
